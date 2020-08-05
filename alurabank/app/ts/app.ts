4a108f54076a943f5cfa9e6503f7ac76095cf9db
Lucas Sabatino <lucas.sabatino@jnmoura.como.br>
2020-08-05 16:27:41 -0300

    Versão até a aula 2 do curso TypeScript Alura - módulo2

---
+++ alurabank/app/ts/app.ts
@@ -0,0 +1,5 @@
+alert("Oi");
+import { NegociacaoController } from './controllers/NegociacaoController'
+const controller = new NegociacaoController();
+
+$(".form").submit(controller.adiciona.bind(controller))
